1c4dd66b8eefbad04baa375c5819829ad414e56c
yuanqing <desk@yuanqing.sg>
2021-08-21 14:12:41 +0800

    Initial commit

---
+++ scripts/print-plugins-table.js
@@ -0,0 +1,25 @@
+import fs from 'fs'
+import { dirname, resolve } from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+function main () {
+  const pluginsFilePath = resolve(__dirname, '..', 'plugins.json')
+  const plugins = JSON.parse(fs.readFileSync(pluginsFilePath, 'utf8'))
+  console.log('Plugin | Description | Author | Installs')
+  console.log(':--|:--|:--|:--')
+  for (const {
+    id,
+    name,
+    description,
+    githubUrl,
+    authorName,
+    authorUrl
+  } of plugins) {
+    console.log(
+      `[**${name}**](${githubUrl}) | ${description} | [${authorName}](${authorUrl}) | [![installs](https://img.shields.io/endpoint?cacheSeconds=1800&url=https://yuanqing.github.io/figma-plugins-stats/plugin/${id}/installs.json)](https://figma.com/community/plugin/${id})`
+    )
+  }
+}
+main()
